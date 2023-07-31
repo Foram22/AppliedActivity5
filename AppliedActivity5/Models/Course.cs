56fac009099b4a02076f3bbc43427f4e885052b3
Foram22 <foramjpatel5778@gmail.com>
2023-07-31 16:18:40 -0400

    added two model classes named student and courses

---
+++ AppliedActivity5/Models/Course.cs
@@ -0,0 +1,7 @@
+﻿public class Course
+{
+    public int Id { get; set; }
+    public string CourseName { get; set; }
+    public string Instructor { get; set; }
+    public string RoomNumber { get; set; }
+}
