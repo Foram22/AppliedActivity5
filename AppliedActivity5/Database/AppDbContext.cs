0105bfd2cbd438b0eed9b4c9b547535cd43f26cc
Foram22 <foramjpatel5778@gmail.com>
2023-07-31 16:26:26 -0400

    database setup is done untill now

---
+++ AppliedActivity5/Database/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppliedActivity5.Database
+{
+	public class AppDbContext : DbContext
+	{
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=app.db");
+        }
+
+        public AppDbContext()
+		{
+		}
+	}
+}
+
---
+++ AppliedActivity5/SeedDate/DatabaseSeeder,cs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using AppliedActivity5.Database;
+
+namespace AppliedActivity5.SeedDate
+{
+	public class DatabaseSeeder_cs
+	{
+        public static void SeedData()
+        {
+            using AppDbContext context = new AppDbContext();
+
+            var students = new List<Student>
+            {
+                new Student { Name = "John Doe", Age = 20, Major = "Computer Science" },
+                new Student { Name = "Jane Smith", Age = 22, Major = "Engineering" },
+                // Add more students here
+            };
+
+            var courses = new List<Course>
+            {
+                new Course { CourseName = "Introduction to Programming", Instructor = "Prof. Johnson", RoomNumber = "101" },
+                new Course { CourseName = "Data Structures", Instructor = "Prof. Williams", RoomNumber = "202" },
+                // Add more courses here
+            };
+
+            context.Students.AddRange(students);
+            context.Courses.AddRange(courses);
+            context.SaveChanges();
+        }
+    }
+}
+
